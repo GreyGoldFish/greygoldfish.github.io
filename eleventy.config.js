1344aaa6ff95400c0244708b711f7b2a134dde7a
Lucas Aquino de Assis <55509085+GreyGoldFish@users.noreply.github.com>
2025-05-12 16:47:28 -0300

    chore: refactor site content

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,12 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default async function(eleventyConfig) {
+    eleventyConfig.addGlobalData("author", "Lucas Aquino de Assis");
     const currentYear = new Date().getFullYear();
     eleventyConfig.addGlobalData("currentYear", currentYear);
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+    eleventyConfig.addLayoutAlias("post", "layouts/posts/default.njk");
+    eleventyConfig.addLayoutAlias("project", "layouts/projects/default.njk");
 };
