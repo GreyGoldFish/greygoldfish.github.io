263bc907426a40754a0acb4a7b4cc294f30097e2
Lucas Aquino de Assis <55509085+GreyGoldFish@users.noreply.github.com>
2025-05-12 14:56:11 -0300

    feat: integrate eleventyNavigation for dynamic nav

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+
 export default async function(eleventyConfig) {
     const currentYear = new Date().getFullYear();
     eleventyConfig.addGlobalData("currentYear", currentYear);
+
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
