a15c20c552b3103d5f7137799ec061a0a8adef59
Lucas Aquino de Assis <55509085+GreyGoldFish@users.noreply.github.com>
2025-05-21 13:28:07 -0300

    feat: restructure project layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,16 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default async function(eleventyConfig) {
+    eleventyConfig.setInputDirectory("src");
+
     eleventyConfig.addGlobalData("author", "Lucas Aquino de Assis");
     const currentYear = new Date().getFullYear();
     eleventyConfig.addGlobalData("currentYear", currentYear);
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    eleventyConfig.addPassthroughCopy("css/tailwind.css");
+
     eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
     eleventyConfig.addLayoutAlias("post", "layouts/posts/default.njk");
     eleventyConfig.addLayoutAlias("project", "layouts/projects/default.njk");
